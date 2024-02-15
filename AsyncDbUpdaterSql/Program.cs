c7965e6059d7a724b4fa1356853883a5997d6ef4
Flavio Souza <flavio.augusto.c.souza@gmail.com>
2024-02-15 00:55:04 -0400

    feat: :sparkles: Segunda versão do Docker compose e Novo Projeto

---
+++ AsyncDbUpdaterSql/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AsyncDbUpdaterSql;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for(var i = 0; i < 20; i++)
+            Console.WriteLine("Hello, World!");
+    }
+}
